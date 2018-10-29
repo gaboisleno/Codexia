68ed12dfd903ed76d6d5930eecf0f43c2c3839f3
gaboisleno <isleno.gabriel@gmail.com>
2018-10-29 10:28:03 -0300

    mejoras

--- todos.js
+++ todos.js
@@ -2,37 +2,40 @@
 async function getJson(url){
     const axios = require('axios');
     const pag = await (axios.get(url)) ;
-    const json = pag.data;
-    return json;
+    return pag.data;
 }
 
 async function getUsrID(email){
     const jsonUsrs = await getJson('https://jsonplaceholder.typicode.com/users');
-
-       const jsonFiltrado = jsonUsrs.filter(jsonUsrs => jsonUsrs.email==email);
-       //const jsonMapeado  = jsonFiltrado.map( jsonElem => {return jsonElem.id}); //
-       return jsonElem[0].id; //
-       //return jsonMapeado[0];
+       const jsonFiltrado = jsonUsrs.find(jsonUsrs => jsonUsrs.email==email);
+       return jsonFiltrado.id;
 }
 
 async function getCompleted(usrID){
     const jsonTodos = await getJson('https://jsonplaceholder.typicode.com/todos');
 
         const jsonFiltrado   = jsonTodos.filter(jsonTodos => jsonTodos.userId==usrID && jsonTodos.completed==true);
-        const totalCompleted = jsonFiltrado.map(jsonElem => {return jsonElem.id });
-        return (totalCompleted.length);
+        return (jsonFiltrado.length);
 }
 
 /****************************************************/
 
 async function main (){
 
+let user = "Sincere@april.biz";
+let idUser = await getUsrID(user);
+let totalCompleted = await getCompleted(idUser);
+
+console.log('El usuario: '+user+' (ID: '+idUser+') tiene '+totalCompleted+' TODOs completados.');
+
+/*
     let user = "Sincere@april.biz"; 
     getUsrID(user).then(v=>{
        getCompleted(v).then(x=>{
            console.log('El usuario: '+user+' (ID: '+v+') tiene '+x+' TODOs completados.');
        });
-    });   
+    });
+*/   
 }
 
 main();
