831f3c544b059e629686e7e636c3e72db77c443b
gaboisleno <isleno.gabriel@gmail.com>
2018-10-30 13:32:27 -0300

    practica

---
+++ albums.js
@@ -0,0 +1,85 @@
+
+//Devuelve el jon de una direccion url
+async function getJson(url){
+    const axios = require('axios');
+    const pag   = await (axios.get(url)) ;
+    return pag.data;
+};
+
+//Devuelve el ID de un usuario
+async function getUsrID(email){
+    const jsonUsrs     = await getJson('https://jsonplaceholder.typicode.com/users');
+    const jsonFiltrado = jsonUsrs.find(jsonUsrs => jsonUsrs.email==email);
+    return jsonFiltrado.id;
+};
+
+//Devuelve todos los almbumes de un usuario
+async function getAlbumsUser(userID){
+    const jsonAlbums    = await getJson('https://jsonplaceholder.typicode.com/albums');
+    const jsonFiltrado = jsonAlbums.filter(jsonAlbums => jsonAlbums.userId==userID);
+    return (jsonFiltrado);
+};
+
+//Dado un id de album, devuelve ese album
+async function getAlbum(albumId){
+    const jsonPhotos   = await getJson('https://jsonplaceholder.typicode.com/photos');
+    const jsonAlbum = jsonPhotos.filter(jsonPhotos => jsonPhotos.albumId==albumId);
+    return (jsonAlbum);
+};
+
+//Crea una nueva carpeta
+async function newFolder(nombre){
+    const Promise = require('bluebird');
+    const fs      = require('fs');
+    const dir     = './'+nombre;
+
+    const mkFolder = Promise.promisify(fs.mkdirSync); 
+    fldr = await mkFolder(dir);
+};
+
+//Dada una url descarga la imagen
+async function dwnImage(url, imgName, folder){
+    const fs      = require('fs');
+    const axios   = require('axios');
+    const Promise = require('bluebird');
+
+    const img     = await (axios.get(url));
+    const saveimg = Promise.promisify(fs.writeFile);
+
+    const conten  = await saveimg(folder+'/'+imgName+'.png', img.data, function(err) {
+        if(err) {return console.log(err)};
+        });
+};
+
+async function createAlbum(userID){
+    let albumes   = await getAlbumsUser(userID);
+    let idAlbumes = albumes.map(element => {return element.id});
+    console.log(idAlbumes);
+
+    //Por cada AlbumId
+    idAlbumes.forEach(async (item)=>{
+
+        //Descargar el album
+        let album = await getAlbum(idAlbumes[item]);
+        console.log(album);
+
+        //Crear una carpeta
+        newFolder(item);
+
+        //Descargar cada foto del album/*
+        album.forEach(async (foto)=>{
+            await dwnImage(foto.url, foto.id, item);
+        }); 
+    });
+};
+
+
+/****************************************************/
+
+async function main (){
+    let user            = "Sincere@april.biz";
+    let idUser          = await getUsrID(user);
+    createAlbum(idUser); 
+};
+
+main();
--- todos.js
+++ todos.js
@@ -1,41 +1,33 @@
 
+//Devuelve el jon de una direccion url
 async function getJson(url){
     const axios = require('axios');
-    const pag = await (axios.get(url)) ;
+    const pag   = await (axios.get(url)) ;
     return pag.data;
-}
+};
 
+//Devuelve el ID de un usuario
 async function getUsrID(email){
-    const jsonUsrs = await getJson('https://jsonplaceholder.typicode.com/users');
-       const jsonFiltrado = jsonUsrs.find(jsonUsrs => jsonUsrs.email==email);
-       return jsonFiltrado.id;
-}
+    const jsonUsrs     = await getJson('https://jsonplaceholder.typicode.com/users');
+    const jsonFiltrado = jsonUsrs.find(jsonUsrs => jsonUsrs.email==email);
+    return jsonFiltrado.id;
+};
 
+//Checkea cuantos campos -completed- tiene un usuario
 async function getCompleted(usrID){
-    const jsonTodos = await getJson('https://jsonplaceholder.typicode.com/todos');
-
-        const jsonFiltrado   = jsonTodos.filter(jsonTodos => jsonTodos.userId==usrID && jsonTodos.completed==true);
-        return (jsonFiltrado.length);
-}
+    const jsonTodos    = await getJson('https://jsonplaceholder.typicode.com/todos');
+    const jsonFiltrado = jsonTodos.filter(jsonTodos => jsonTodos.userId==usrID && jsonTodos.completed==true);
+    return (jsonFiltrado.length);
+};
 
 /****************************************************/
 
 async function main (){
-
-let user = "Sincere@april.biz";
-let idUser = await getUsrID(user);
-let totalCompleted = await getCompleted(idUser);
-
-console.log('El usuario: '+user+' (ID: '+idUser+') tiene '+totalCompleted+' TODOs completados.');
-
-/*
-    let user = "Sincere@april.biz"; 
-    getUsrID(user).then(v=>{
-       getCompleted(v).then(x=>{
-           console.log('El usuario: '+user+' (ID: '+v+') tiene '+x+' TODOs completados.');
-       });
-    });
-*/   
-}
+    let user            = "Sincere@april.biz";
+    let idUser          = await getUsrID(user);
+    
+    let totalCompleted  = await getCompleted(idUser);
+    console.log('El usuario '+user+' (ID: '+idUser+') tiene '+totalCompleted+' TODOs completados.');  
+};
 
 main();
